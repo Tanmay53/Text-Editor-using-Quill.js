793402d63bf2ab7fe1dd9b0ccb936802d277a1b7
Tanmay <tanmaykhatri28@gmail.com>
2019-10-29 17:48:16 +0530

    Redefinition
    Storing of lnKepr and curLine redefined and simplified.

--- Quill_Controller.js
+++ Quill_Controller.js
@@ -10,11 +10,12 @@ var prCursorPos = 0;
 var lineCount = 1;//tmp
 var lnKepr = new Array();
 var curLine = 1;
+var nIndntKepr = new Array();
 lnKepr[0] = 0;
+nIndntKepr[0] = 0;
+//var selIndex = ;
+var selLength = 0;
 quill.on("editor-change", function(eventName, chA, chB, source) {
-    var uSelect = quill.getSelection();
-    //var lIndex = uSelect.index;
-    //var selLength = uSelect.length;
     //console.log(/*lIndex +*/ ":" + selLength);
     //let [line, offset] = quill.getLine(5);
     //let index = quill.getIndex(line);
@@ -22,70 +23,51 @@ quill.on("editor-change", function(eventName, chA, chB, source) {
         var delta = chA;
         var oldDelta = chB;
         var retainh = ((delta.ops[0].retain != undefined) ? delta.ops[0].retain : 0);
-        var deltaTxt = (((cursorPos == 1) || (cursorPos == 0)) ? delta.ops[0].insert : delta.ops[1].insert);//errrrrr for 0 position.
-        var tlk = 0;//tmp
-        var lnHold = new Array();
+        var deltaTxt = (((cursorPos == 1) || (cursorPos == 0)) ? delta.ops[0].insert : delta.ops[1].insert);
         if(deltaTxt != undefined) {
             var dTxtLn = deltaTxt.length;
-            while(deltaTxt.indexOf("\n") != -1) {
-                var lnIndx = deltaTxt.indexOf("\n");
-                lineCount += 1;
-                lnHold[tlk] = ((tlk == 0) ? (retainh + lnIndx) : (lnHold[tlk-1] + lnIndx + 1));
-                console.log("Reatin & ln : " + retainh + " : " + lnIndx);
-                tlk += 1;
-                deltaTxt = deltaTxt.substr(lnIndx + 1);
+            var incStart = lnKepr.findIndex(function(elem) { return elem >= retainh; });
+            for(var inc = incStart; inc < lnKepr.length; inc++) {
+                lnKepr[inc] += dTxtLn;
             }
-            var ldr = ((lnKepr.indexOf(retainh) == -1) ? 0 : 1);
-            console.log("ldr : " + ldr);
-            var maxLn = lnKepr.findIndex(function(ln) {
-                return ln > retainh;
-            });
-            var l = 0;
-            for(l = 0; l < tlk; l++) {
-                lnKepr.splice(((maxLn != -1) ? (maxLn + l) : lnKepr.length), 0, lnHold[l]);
+            var deltaTxt1 = deltaTxt;
+            var lngLn = 0;
+            while(deltaTxt1.indexOf("\n") != -1) {
+                var shLn = deltaTxt1.indexOf("\n");
+                lnKepr.splice(incStart++, 0, (retainh + shLn + lngLn));
+                lngLn += shLn +  1;
+                deltaTxt1 = deltaTxt1.substr(shLn + 1);
+                lineCount++;
             }
-            if(ldr && (tlk > 0)) {
-                var flHold = lnKepr[maxLn - 1];
-                lnKepr.splice(maxLn - 1, 1);
-                lnKepr.splice(maxLn + l -1, 0, flHold);
-            }
-            l = l - ldr;
-            while(((maxLn + l) < lnKepr.length) && (maxLn != -1)) {
-                lnKepr[maxLn + l] += dTxtLn;
-                l++;
-            }
-            console.log("maxLn : " + maxLn + " : " + JSON.stringify(lnKepr) + " : " + l);
-            console.log("Entered : " + JSON.stringify(lnHold));
         }
         else {
             var delno = ((cursorPos == 0) ? delta.ops[0].delete : delta.ops[1].delete);
-            console.log("No. of deletions : " + delno);
-            var delStr = oldDelta.ops[0].insert.substr(delta.ops[0].retain, delno);
-            while(delStr.indexOf("\n") != -1) {
-                var idk = delStr.indexOf("\n");
-                lineCount -= 1;
-                delStr = delStr.substr(idk + 1);
-            }
-            var delElem = lnKepr.filter(function(elem) {
-                return ((elem >= retainh) && (elem < (retainh + delno)));
-            });
-            lnKepr.splice(lnKepr.indexOf(delElem[0]), delElem.length);
-            var decStart = lnKepr.findIndex(function(elem) { return retainh < elem; });
-            for(var dec = decStart; dec < lnKepr.length; dec++) {
+            var delAr = lnKepr.filter(function(elem) { return ((elem >= retainh) && (elem < (retainh + delno))); });
+            var delArLn = delAr.length;
+            var delStart = lnKepr.findIndex(function(elem) { return elem >= retainh; });
+            lnKepr.splice(delStart, delArLn);
+            lineCount += delArLn;
+            for(var dec = delStart; dec < lnKepr.length; dec++) {
                 lnKepr[dec] -= delno;
             }
-            console.log(JSON.stringify(lnKepr));
         }
-        console.log(JSON.stringify(delta) + " : " + JSON.stringify(oldDelta) + " : " + JSON.stringify(source)/* + " : " + delta.ops[1].delete*/); //JSON.stringify(delta.ops[0].retain)
+        curLine = (lnKepr.findIndex(function(elem) { return elem >= cursorPos; })) + 1;
+        console.log("Current Line : " + curLine);
+        console.log(JSON.stringify(lnKepr));
+        console.log(JSON.stringify(delta) + " : " + JSON.stringify(oldDelta) + " : " + JSON.stringify(source)); //JSON.stringify(delta.ops[0].retain)
         console.log("LineCount : " + lineCount);
     }
     if(eventName == "selection-change") {
+        var uSelect = quill.getSelection();
+        //selIndex = uSelect.index; Simillar to cursorPos.
+        selLength = ((uSelect != undefined) ? uSelect.length : 0);//whenever selected text is required, the length and the index can be used on the oldDelta
         var range = chA;
         var oldRange = chB;
         cursorPos = ((range != null) ? range.index : cursorPos);
         prCursorPos = ((oldRange != null) ? oldRange.index : prCursorPos);
-        curLine = lnKepr.findIndex(function(elem) { return elem >= cursorPos; });
-        curLine = ((curLine == -1) ? lnKepr.length : ((curLine == 0) ? 1 :curLine));
+        tcurLine = (lnKepr.findIndex(function(elem) { return elem >= cursorPos; })) + 1;
+        curLine = ((tcurLine == 0) ? curLine : tcurLine);
+        console.log("Length : " + selLength/* + " Index : " + selIndex*/);
         console.log(JSON.stringify(range) + " : " + JSON.stringify(oldRange) + " : " + JSON.stringify(source)); //JSON.stringify(delta.ops[0].retain)
         console.log(cursorPos + " : " + prCursorPos);
         console.log("Current Line : " + curLine);
